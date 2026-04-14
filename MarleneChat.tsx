58018b06a6ea99ba419cefb33ff4d8e15905837a
Adilson <adilsonjuetheo@gmail.com>
2026-04-14 09:24:29 -0300

    auto: salvar alterações

--- MarleneChat.tsx
+++ MarleneChat.tsx
@@ -238,10 +238,11 @@ const s = StyleSheet.create({
     position: 'absolute',
     top: 0, left: 0, right: 0, bottom: 0,
     backgroundColor: 'rgba(26,18,8,0.5)',
-    justifyContent: 'flex-end',
     zIndex: 999,
   },
   container: {
+    position: 'absolute',
+    left: 0, right: 0,
     backgroundColor: CORES.parch,
     borderTopLeftRadius: 24,
     borderTopRightRadius: 24,
